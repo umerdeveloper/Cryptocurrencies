b8b8a8af84afcbcdf3e2cdf406e290650c56f729
Umer Khan <umar_s66@outlook.com>
2020-12-28 17:48:57 +0500

    icon added

--- CryptoPredictor/View/ContentView.swift
+++ CryptoPredictor/View/ContentView.swift
@@ -18,35 +18,67 @@ struct ContentView: View {
         Coin(id: "ETH", name: "Ethereum", price: "300.9", icon: "ethereum")
     ]
     
+    var myWallet: [Coin] = [
+        Coin(id: "XIP", name: "Ripple", price: "0.90", icon: "ripple"),
+        Coin(id: "TRX", name: "Tron", price: "98", icon: "tron"),
+        Coin(id: "ETH", name: "Ethereum", price: "300.9", icon: "ethereum")
+    ]
+    
     var body: some View {
         NavigationView {
             
             VStack {
                 
+                Image(systemName: "waveform.path.ecg")
+                    .resizable()
+                    .frame(width: 120, height: 100)
+                    .padding()
+                    .foregroundColor(.orange)
+                
                 Text("Your Crypto Balance")
+                    .foregroundColor(.orange)
                 Text("$2,22.1")
                     .font(.largeTitle)
                     .fontWeight(.heavy)
+                    .foregroundColor(.orange)
                 
                 
                 List {
-                    Section(header: Text("Current Rates")) {
-                        ForEach(rates) { coin in
+                    Section(header: Text("My Wallet")) {
+                        ForEach(myWallet) { coin in
                             HStack {
                                 Image(coin.icon)
                                     .resizable()
                                     .frame(width: 32, height: 32)
-                                Text(coin.name)
+                                Text("\(coin.name) - \(coin.id)")
+                                
+                                Spacer()
+                                
+                                Text("$\(coin.price)")
+                                    .fontWeight(.bold)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Current Rates")) {
+                        ForEach(rates) { coin in
+                            
+                            NavigationLink(destination: Text("Hello")) {
+                                HStack {
+                                    Image(coin.icon)
+                                        .resizable()
+                                        .frame(width: 32, height: 32)
+                                    Text("\(coin.name) - \(coin.id)")
+                                    
+                                    Spacer()
+                                    
+                                    Text("$\(coin.price)")
+                                        .fontWeight(.bold)
+                                }
                             }
                         }
                     }
                 }
-                
-                
-                
-                
-                
-                
             }
             .navigationBarTitle(Text("Dashboard"))
             .navigationBarHidden(true)
